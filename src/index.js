ea528d5206e430b82715253b74b8b3d8cc80a709
Maria Lee <juggleisreal@gmail.com>
2024-04-08 17:17:31 -0400

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client'; // Import createRoot from react-dom/client
+import ReactDOM from 'react-dom/client';
 
 import App from './App';
 
-// Use createRoot instead of ReactDOM.render
-createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+
+
